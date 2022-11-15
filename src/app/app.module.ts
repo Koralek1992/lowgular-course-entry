802af2e8ac356afc0e7688225dd828685207cc09
Koralek1992 <dkokralek2@gmail.com>
2022-11-15 10:50:14 +0100

    pobranie zdjęć

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EmployeeListComponentModule} from "./ui/employee-list/employee-list.component-module";
+import {FacesComponentModule} from "./ui/faces/faces.component-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,7 @@ import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.co
     AppRoutingModule,
     HttpClientModule,
     EmployeeListComponentModule,
+    FacesComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
