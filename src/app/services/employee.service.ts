20ed258aedb5e3f7d84ed8e5e16da1edd836d86d
Koralek1992 <dkokralek2@gmail.com>
2022-11-17 13:18:18 +0100

    create new method delete

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
-import {CreateEmployeeModel} from "../model/create-employee.model";
+import {Observable} from 'rxjs';
+import {PersonModel} from '../model/person.model';
+import {CreateEmployeeModel} from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -15,5 +15,10 @@ export class EmployeeService {
 
   create(employee: CreateEmployeeModel): Observable<any> {
     return this._httpClient.post('https://dummy.restapiexample.com/create', employee);
+  }
+
+  delete(id: string): Observable<any> {
+    return this._httpClient.delete('https://dummy.restapiexample.com/delete' + id);
+
   }
 }
