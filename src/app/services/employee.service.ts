5e5180dac2d7ebc724c670b3ad6e1b4bdc547de8
Koralek1992 <dkokralek2@gmail.com>
2022-11-23 13:49:29 +0100

    change params on details

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {PersonModel} from '../model/person.model';
+import {ApiResponse} from './api.response';
+import {EmployeeResponse} from '../model/employee.response';
 import {CreateEmployeeModel} from '../model/create-employee.model';
-import {ApiResponse} from "./api.response";
-import {EmployeeResponse} from "../model/employee.response";
+import {EmployeeModel} from '../model/employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -34,5 +36,15 @@ export class EmployeeService {
   delete(id: string): Observable<any> {
     return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete' + id);
 
+  }
+
+  getOne(id: string): Observable<EmployeeModel> {
+    return this._httpClient.get<ApiResponse<EmployeeResponse>>('https://dummy.restapiexample.com/api/v1/employee/' + id).pipe(
+      map((response: ApiResponse<EmployeeResponse>): EmployeeModel => ({
+        id: response.data.id,
+        image: response.data.profile_image,
+        email: '',
+        name: response.data.employee_name
+      })))
   }
 }
